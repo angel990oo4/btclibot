843c9b544683ee281516b52692b141eafc910c64
angel990oo4 <angel.1oo4.dev@gmail.com>
2022-10-20 12:48:05 -0400

    done stake uid command

--- commands/btcli.js
+++ commands/btcli.js
@@ -1,29 +1,67 @@
 const {
-    SlashCommandBuilder,
-    ActionRowBuilder,
-    SelectMenuBuilder,
-    ModalBuilder,TextInputBuilder,
-    TextInputStyle,
-  } = require("discord.js");
-  const wait = require("node:timers/promises").setTimeout;
-  
-  module.exports = {
-    data: new SlashCommandBuilder()
-      .setName("btcli")
-      .setDescription("Bittensor Discord Cli")
-      .addStringOption(option =>
-        option.setName('mechanism')
-            .setDescription('command')
-            .setRequired(true)
-            .addChoices({ name: 'Stake', value: 'stake' }))
-      .addStringOption(option =>
-        option.setName('uid')
-            .setDescription('UID for Stake value')
-            .setRequired(true)),
+  SlashCommandBuilder,
+  ActionRowBuilder,
+  SelectMenuBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} = require('discord.js');
 
+const axios = require('axios');
 
-      async execute(interaction) {
-        await interaction.reply('Pong')
+const wait = require('node:timers/promises').setTimeout;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('btcli')
+    .setDescription('Bittensor Discord Cli')
+    .addStringOption((option) =>
+      option
+        .setName('mechanism')
+        .setDescription('mechanism')
+        .setRequired(true)
+        .addChoices({ name: 'stake', value: 'stake' })
+    )
+    .addStringOption((option) =>
+      option
+        .setName('uid')
+        .setDescription('UID for Stake value')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const mechanism = interaction.options.getString('mechanism');
+    const uid = interaction.options.getString('uid');
+    console.log('mechanism', mechanism);
+    console.log('uid', uid);
+    await interaction.deferReply({ ephemeral: false });
+
+    if (mechanism === 'stake') {
+      if (
+        !Number.isInteger(Number(uid)) ||
+        Number(uid) < 0 ||
+        Number(uid) > 4095
+      ) {
+        await interaction.editReply(
+          `UID should be an integer between 0 and 4095`
+        );
+      } else {
+        await axios
+          .get('https://arcane-mesa-86933.herokuapp.com/api/neuron')
+          .then(async (res) => {
+            await interaction.editReply(
+              `${res.data.neuron[Number(uid)].stake}`,
+              {
+                ephemeral: false,
+              }
+            );
+          })
+          .catch(async (err) => {
+            await interaction.editReply(`${err}`);
+          });
+      }
+    } else {
+      await interaction.editReply(`Unsupported mechanism`);
     }
-  };
-  
+  },
+};
