5e111405939deb7f0c5ec132575fcf318b44e5c0
angel990oo4 <angel.1oo4.dev@gmail.com>
2022-10-20 22:27:56 -0400

    added cors option

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-
-app.use(cors());
+var corsOptions = {
+  origin: '*',
+};
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
