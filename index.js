e3abc082ee4d77a5d122fdea855ab43e9e654daf
angel990oo4 <angel.1oo4.dev@gmail.com>
2022-10-31 07:56:24 -0400

    added raw incentive data in csv file

--- index.js
+++ index.js
@@ -263,6 +263,32 @@ client.on('messageCreate', async (msg) => {
         });
       });
   }
+  if (msg.content === '$btcli incentive --raw') {
+    const message = await msg.channel.send({ content: 'loading data...' });
+    requestData()
+      .then(async (res) => {
+        await message.delete();
+        let NeuronData = res?.data?.neuron;
+        const csvContent = NeuronData.map((neuron, index) =>
+          [`${neuron.uid}`, `${neuron.incentive / 18446744073709551615}`].join(
+            ','
+          )
+        ).join('\n');
+
+        console.log('csvContent', csvContent);
+        const buffer = Buffer.from(csvContent, 'utf-8');
+        const file = new AttachmentBuilder(buffer, { name: 'incentive.csv' });
+        msg.channel.send({
+          content: 'Incentive raw value',
+          files: [file],
+        });
+      })
+      .catch((err) => {
+        msg.channel.send({
+          content: `${err}`,
+        });
+      });
+  }
 });
 
 client.login(token);
