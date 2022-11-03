cddbc76894de2065a5032a2d45d41d34aeb81ef6
angel990oo4 <angel.1oo4.dev@gmail.com>
2022-11-03 13:16:00 -0400

    updated bitcli commands list

--- const/btclicommands.js
+++ const/btclicommands.js
@@ -1,10 +1,13 @@
 const BtcliCommands =
-  '**$btcli stake [uid]**: The stake value for a given UID \n\
+  '**$btcli --help**: btcli commands list\n\
+**$btcli stake [uid]**: The stake value for a given UID \n\
 **$btcli inspect  [uid]**: Neuron info for a given UID \n\
-**$btcli emission**: The plot of sorted emission value \n\
+**$btcli metagraph --raw**: The csv file of neuron info \n\
 **$btcli incentive**: The plot of sorted incentive value \n\
-**$btcli incentive --raw**: The csv file of incentive valu` \n\
+**$btcli incentive --raw**: The csv file of incentive value \n\
 **$btcli incentive --raw [--[ascending/descending]]**: The csv file of (optionally) sorted incentive values \n\
+**$btcli emission**: The plot of sorted emission value \n\
+**$btcli emission --raw**: The csv file of incentive value \n\
 ';
 
 module.exports = {
