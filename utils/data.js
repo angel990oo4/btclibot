9a2f4295e585b05023577aff63ad819722b1adaf
angel990oo4 <angel.1oo4.dev@gmail.com>
2022-11-18 13:24:18 -0500

    added maxContentLength, maxBodyLength infinity

--- utils/data.js
+++ utils/data.js
@@ -1,19 +1,28 @@
 const axios = require('axios');
 const requestData = async () => {
   const response = await axios.get(
-    'https://arcane-mesa-86933.herokuapp.com/api/bot'
+    'https://arcane-mesa-86933.herokuapp.com/api/bot',
+    {
+      maxContentLength: Infinity,
+      maxBodyLength: Infinity,
+    }
   );
   return response;
 };
 
 const requestChain = async () => {
-  const response = await axios.get('http://127.0.0.1:5000/bot');
+  const response = await axios.get('http://127.0.0.1:5000/bot', {
+    maxContentLength: Infinity,
+    maxBodyLength: Infinity,
+  });
   return response;
 };
 
 const requestHistory = async (params) => {
   const response = await axios.get('http://127.0.0.1:5000/subtensorapi', {
     data: params,
+    maxContentLength: Infinity,
+    maxBodyLength: Infinity,
   });
   return response;
 };
