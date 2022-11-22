b0a5153f347f362b3efe6b707f78261df9a4ba89
angel990oo4 <angel.1oo4.dev@gmail.com>
2022-11-22 16:51:19 -0500

    added embed in inspectExecute

--- btcli/inspect.js
+++ btcli/inspect.js
@@ -1,5 +1,6 @@
 const { requestData } = require('../utils/data');
 const { validUID } = require('../utils/utils');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   async inspectExecute(uid, msg) {
@@ -14,26 +15,34 @@ module.exports = {
           await message.delete();
           let NeuronData = res?.data?.neuron;
           if (NeuronData?.[Number(uid)]?.stake) {
+            const inspectEmbed = new EmbedBuilder()
+              .setTitle(`The neuron data of UID ${uid}`)
+              .setDescription(
+                `**hotkey**: ${
+                  NeuronData[Number(uid)].hotkey
+                }\n  **coldkey** : ${
+                  NeuronData[Number(uid)].coldkey
+                }\n  **stake**: ${
+                  NeuronData[Number(uid)].stake / 1000000000
+                }\n  **rank**: ${
+                  NeuronData[Number(uid)].rank / 18446744073709551615
+                }\n  **trust**: ${
+                  NeuronData[Number(uid)].trust / 18446744073709551615
+                }\n  **consensus**: ${
+                  NeuronData[Number(uid)].consensus / 18446744073709551615
+                }\n  **incentive**: ${
+                  NeuronData[Number(uid)].incentive / 18446744073709551615
+                }\n  **dividends**: ${
+                  NeuronData[Number(uid)].dividends / 18446744073709551615
+                }\n  **emission**: ${
+                  NeuronData[Number(uid)].emission / 1000000000
+                }\n  **active**: ${
+                  NeuronData[Number(uid)].active ? 'true' : 'false'
+                }\n`
+              );
+
             msg.channel.send({
-              content: `Uid: ${uid}\n{\n  hotkey: ${
-                NeuronData[Number(uid)].hotkey
-              }\n  coldkey : ${NeuronData[Number(uid)].coldkey}\n  stake: ${
-                NeuronData[Number(uid)].stake / 1000000000
-              }\n  rank: ${
-                NeuronData[Number(uid)].rank / 18446744073709551615
-              }\n  trust: ${
-                NeuronData[Number(uid)].trust / 18446744073709551615
-              }\n  consensus: ${
-                NeuronData[Number(uid)].consensus / 18446744073709551615
-              }\n  incentive: ${
-                NeuronData[Number(uid)].incentive / 18446744073709551615
-              }\n  dividends: ${
-                NeuronData[Number(uid)].dividends / 18446744073709551615
-              }\n  emission: ${
-                NeuronData[Number(uid)].emission / 1000000000
-              }\n  active: ${
-                NeuronData[Number(uid)].active ? 'true' : 'false'
-              }\n}`,
+              embeds: [inspectEmbed],
             });
           } else {
             await message.delete();
