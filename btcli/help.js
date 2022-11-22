1fb0a64358f544ce9649864964b416cc95f18a99
angel990oo4 <angel.1oo4.dev@gmail.com>
2022-11-22 04:28:40 -0500

    updated helper description, added embed option

--- btcli/help.js
+++ btcli/help.js
@@ -1,9 +1,13 @@
 const { BtcliCommands } = require('../const/btclicommands');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   async helpExecute(msg) {
+    const helperEmbed = new EmbedBuilder()
+      .setTitle('Btcli Commands')
+      .setDescription(`${BtcliCommands}`);
     msg.channel.send({
-      content: `${BtcliCommands}`,
+      embeds: [helperEmbed],
     });
   },
 };
--- const/btclicommands.js
+++ const/btclicommands.js
@@ -1,17 +1,17 @@
 const BtcliCommands =
-  '**$btcli --help**: *btcli commands list*\n\
-**$btcli stake --uid [uid]**: *The stake value for a given UID* \n\
-**$btcli inspect --uid [uid]**: *Neuron info for a given UID* \n\
-**$btcli metagraph --raw**: *The csv file of neuron info* \n\n\
-**$btcli incentive**: The plot of sorted incentive value* \n\
-**$btcli incentive --raw**: *The csv file of incentive value* \n\
-**$btcli incentive --raw [--[ascending/descending]]**: *The csv file of (optionally) sorted incentive values* \n\
-*Same commands for emission, rank, trust, consensus, dividends*\n\n\
-**$btcli block**: *Current block number*\n\
-**$btcli difficulty**: *Difficulty value*\n\
-**$btcli issuance**: *Total issuance*\n\
-**$btcli next_halving**: *The left time until next halving*\n\
-**$btcli generate**: *Bittensor reponse bot*\n\
+  '**$btcli --help**: btcli commands list\n\
+**$btcli stake --uid [uid]**: The stake value for a given UID \n\
+**$btcli inspect --uid [uid]**: Neuron info for a given UID \n\
+**$btcli metagraph --raw**: The csv file of neuron info \n\n\
+**$btcli incentive**: The plot of sorted incentive value \n\
+**$btcli incentive --raw**: The csv file of incentive value \n\
+**$btcli incentive --raw [--[ascending/descending]]**: The csv file of (optionally) sorted incentive values \n\
+Same commands for emission, rank, trust, consensus, dividends\n\n\
+**$btcli block**: Current block number\n\
+**$btcli difficulty**: Difficulty value\n\
+**$btcli issuance**: Total issuance\n\
+**$btcli next_halving**: The left time until next halving\n\
+**$btcli generate**: Bittensor reponse bot\n\
 ';
 
 const factors = [
