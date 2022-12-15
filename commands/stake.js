4c77a2188f3fa48eb317566a8d69f2a8f051fb6e
angel990oo4 <angel.1oo4.dev@gmail.com>
2022-12-15 12:52:48 -0500

    added stake command

---
+++ commands/stake.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const { requestData } = require('../utils/data');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('stake')
+    .setDescription('Querying the stake value for a specific uid')
+    .addStringOption((option) =>
+      option
+        .setName('--uid')
+        .setDescription('enter the uid number')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    await interaction.deferReply({ ephemeral: true });
+    const uid = interaction.options.getString('--uid') ?? '0';
+    requestData()
+      .then(async (NeuronData) => {
+        if (NeuronData?.data?.neuron?.[Number(uid)]?.stake) {
+          const stakeEmbed = new EmbedBuilder()
+            .setColor('#4caf50')
+            .setDescription(
+              `UID:**${uid}** has **τ${
+                NeuronData?.data?.neuron?.[Number(uid)].stake / 1000000000
+              }** staked `
+            );
+          await interaction.editReply({ embeds: [stakeEmbed] });
+        } else {
+          const errorEmbed = new EmbedBuilder()
+            .setColor(0xee0000)
+            .setDescription(`⚠️ No data found`);
+          await interaction.editReply({ embeds: [errorEmbed] });
+        }
+      })
+      .catch(async (err) => {
+        console.log('err', err);
+        await message.delete();
+        const errorEmbed = new EmbedBuilder()
+          .setColor(0xee0000)
+          .setDescription(`⚠️ No data found`);
+        await interaction.editReply({ embeds: [errorEmbed] });
+      });
+  },
+};
